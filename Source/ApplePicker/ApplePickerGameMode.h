78a2064a2d153bc5eee446dece0a6cd80bf80f38
Yarik <cahloyaroslav@gmail.com>
2023-11-16 15:32:49 +0200

    Add project

---
+++ Source/ApplePicker/ApplePickerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ApplePickerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AApplePickerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AApplePickerGameMode();
+};
+
+
+
